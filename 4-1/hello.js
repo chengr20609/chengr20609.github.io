912ca02e139265bd315f89eaf3697a619454e0fb
Chengr20609 <1796236465@qq.com>
2016-05-11 12:37:31 +0800

    javascript练习

---
+++ 4-1/hello.js
@@ -0,0 +1,19 @@
+var age =70;
+if (age > 30) {
+    if (age >= 70) { alert("青春年华"); } else {
+        alert("四十不惑");
+    }
+} else {
+    alert("人生的路还很长");
+}
+
+for (var i = 0; i < 100; i++) {
+    console.log(i)
+        // 在控制台输出信息
+}
+
+function sayHello(name) {
+    console.log(name + ", 你好");
+}
+
+sayHello("成锐");
